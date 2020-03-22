6c334c6b852d7628857c2eadf674041588dcf028
feres2005 <58883911+feres2005@users.noreply.github.com>
2020-03-22 10:45:56 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+let pets =  [
+    {name : "max" ,type: "dog", bornOn:2018} ,                             
+    {name : "angel" ,type: "cat", bornOn:2015},
+    {name : "jaspere" ,type: "dog", bornOn:2016}];
+
+  const getAge(pet) {
+
+      return new Date().getFullYear - pet.bornOn;
+  }  
+  let petsWithAge = [];
+  pets.forEach(persone=>{
+    let pet = pets[i];
+    pet.age = getAge(pet);
+  
+    petsWithAge.push(pet);
+  }
+  console.log(petsWithAge);
+  let dogs = [];
+ return pet.find(dog=>  {
+   
+      dogs.push(pet);
+    }
+  
+  console.log(dogs);
+  
+  let jasper;
+  return pet.find(jasper=> {
+    let pet = pets[i];
+     {
+      jasper = pet;
+    }
+  }
+console.log("Jasper is " + "jasper.age"+ " years old");
